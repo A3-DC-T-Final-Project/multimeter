d15117ad6c14b6d6b859bcfdb5017730a78409c6
Giuseppe Barillari <joe2k01dev@gmail.com>
2022-03-24 18:45:51 +0100

    multimeter: Make Common a namespace
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- src/common.hpp
+++ src/common.hpp
@@ -1,9 +1,8 @@
 #ifndef _COMMON_H_
 #define _COMMON_H_
 
-class Common {
-    public:
-        static float map(float x, float in_min, float in_max, float out_min, float out_max);
+namespace Common {
+    float map(float x, float in_min, float in_max, float out_min, float out_max);
 };
 
 #endif /* _COMMON_H_ */
